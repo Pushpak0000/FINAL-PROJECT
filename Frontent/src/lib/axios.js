4a87111f34e80f41b41fceefc998d64c41a453a8
Pushpak0000 <bca2005wala@gmail.com>
2025-04-30 12:41:29 +0530

    changes in url

--- Frontent/src/lib/axios.js
+++ Frontent/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.MODE === "development" ? "http://localhost:3000/api" : "/api",
+  baseURL: import.meta.MODE === "development" ? "http://localhost:3000/api" : "https://your-blog-4xnf.onrender.com/api",
   withCredentials:true
 })
 
